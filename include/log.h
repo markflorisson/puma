e1e4b3b9b634a0fe082022363546303c786c51e6
Balakrishnan Nikilesh Dharmaraj <s1126390@ness.epcc.ed.ac.uk>
2011-11-02 23:36:43 +0000

    added timestamp for log

--- include/log.h
+++ include/log.h
@@ -2,17 +2,35 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
+
+static char *
+getTimeStamp(char *timeStamp)
+{
+	time_t currentTime;
+
+	currentTime = time(0);
+	strftime(timeStamp,9,"%H:%M:%S",localtime(&currentTime));
+
+	return(timeStamp);
+}
+
+
 
 static void
 debug_msg(const char* msg, ...)
 {
-    va_list list;
+	va_list list;
+	char timeStamp[16] = {'\0'};
+	
+	getTimeStamp(timeStamp);
 
-    va_start(list,msg);
-    #ifdef DEBUG
-    vfprintf(stdout,msg,list);
-    fflush(stdout);
-    #endif
+	va_start(list,msg);
+#ifdef DEBUG
+	fprintf(stdout,"%s:",timeStamp);
+	vfprintf(stdout,msg,list);
+	fflush(stdout);
+#endif
 
 	va_end(list);
 }
@@ -20,11 +38,17 @@ debug_msg(const char* msg, ...)
 static void
 error_msg(const char* msg, ...)
 {
-    va_list list;
+	va_list list;
+	char timeStamp[16] = {'\0'};
+	
+	getTimeStamp(timeStamp);
 
-    va_start(list,msg);
-    vfprintf(stderr,msg,list);
-    va_end(list);
+	va_start(list,msg);
+
+	fprintf(stderr,"%s:",timeStamp);
+	vfprintf(stderr,msg,list);
+
+	va_end(list);
 }
 
 #endif
