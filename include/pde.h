ff83bf5a7543bb8b8b85da5a6feab2f6f939f306
Shun Liang <shun.liang.public@gmail.com>
2011-11-02 16:39:20 +0000

    Change the function signiture of the computation kernel in the header.

--- include/pde.h
+++ include/pde.h
@@ -1,2 +1,3 @@
-void compute(double hare[NX][NY], double puma[NX][NY], int land[NX][NY], int nx, int ny, int delta_t, int r, 
-	int a, int k, int b, int m, int l);
+void compute(double hare[NX][NY], double puma[NX][NY], int land[NX][NY], int nx, int ny, double delta_t, double r, 
+    double a, double k, double b, double m, double l) 
+
