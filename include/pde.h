9374cbc8e5dfbadbe9329be33537d18d904ca02c
Shun Liang <shun.liang.public@gmail.com>
2011-11-03 16:35:50 +0000

    Change the arguments of the compute function.

--- include/pde.h
+++ include/pde.h
@@ -1,3 +1,1 @@
-void compute(double hare[NX][NY], double puma[NX][NY], int land[NX][NY], int nx, int ny, double delta_t, double r, 
-    double a, double k, double b, double m, double l) 
-
+void compute(double hare[NX][NY], double puma[NX][NY], int land[NX][NY], int nx, int ny, double delta_t, EquationVariables *eq_val) 
