6955c73f7d8e2a0df056665a3740b8373e413191
Mark Florisson <markflorisson88@gmail.com>
2011-11-06 21:09:34 +0000

    Fix kernel test segfault & better output

--- test/test.c
+++ test/test.c
@@ -15,15 +15,33 @@
                         do {\
                             int puma_errno = expr; \
                             if (puma_errno) {\
-                                fputs(puma_strerror(puma_errno), stderr);\
-                                CU_FAIL("");\
+                                fprintf(stderr, "\n    !!! %s !!!\n", puma_strerror(puma_errno));\
+                                CU_FAIL(#expr);\
                                 return;\
                             }\
                         } while (0)
 
-#define assert_int_equals(a, b) do { if (a != b) { CU_ASSERT_EQUAL(a, b); fprintf(stderr, "val1=%d val2=%d", a, b); } } while (0)
+#define assert_int_equals(a, b) do {\
+        if (a != b) {\
+            CU_ASSERT_EQUAL(a, b);\
+            fprintf(stderr, "\n    !!! val1=%d val2=%d", a, b);\
+        }\
+    } while (0)
 
 int map[NX][NY] = {{0}};
+REAL solution[NX][NY] = {{0}};
+REAL hare[NX][NY] = {{0}};
+REAL puma[NX][NY] = {{0}};
+
+static int
+puma_open(const char *filename, const char *mode, FILE **file)
+{
+    *file = fopen(filename, mode);
+    if (!*file)
+        return PUMA_OSERROR;
+
+    return PUMA_NOERR;
+}
 
 /* Pointer to the file used by the tests. */
 static FILE* temp_file = NULL;
@@ -87,70 +105,55 @@ void test_readmap(void)
 
 void test_kernel(void)
 {
-  int nx,ny;
-  int i,j;
-  int value_s,value_p,value_h,value_m;
-  int max_iter=100;
-  char filename_Solution[64] = {"solution.dat"};
-  char filename_Hare[64] = {"Hare.dat"};
-  char filename_Puma[64] = {"Puma.dat"};
-  char filename_Map[64] = {"Map.dat"};
-  EquationVariables eqn_obj;
+    int nx, ny;
+    int i, j;
+    int value_s, value_p, value_h, value_m;
+    int max_iter=100;
+    EquationVariables eqn_obj;
 
-  FILE *file_Solution = fopen(filename_Solution, "r");
-  FILE *file_Hares = fopen(filename_Hare, "r");
-  FILE *file_Puma = fopen(filename_Puma, "r");
-  FILE *file_Map = fopen(filename_Map, "r");
+    FILE *file_solution, *file_hares, *file_puma, *file_map;
 
-  eqn_obj.time_interval= 0.4;
-  eqn_obj.prey_pop_inc_rate = 0.08;
-  eqn_obj.pred_rate_coeff = 0.04;
-  eqn_obj.rep_rate_pred = 0.02;
-  eqn_obj.pred_mort_rate = 0.06;
-  eqn_obj.diff_rate_hares = 0.2;
-  eqn_obj.diff_rate_pumas = 0.2;
+    fail_on_error(puma_open("test/solution.dat", "r", &file_solution));
+    fail_on_error(puma_open("test/Hare.dat", "r", &file_hares));
+    fail_on_error(puma_open("test/Puma.dat", "r", &file_puma));
+    fail_on_error(puma_open("test/Map.dat", "r", &file_map));
 
-  //fscanf(file_Map, "%d %d", &nx, &ny);
-  nx=4;
-  ny=4;
-  REAL solution[NX][NY];
-  REAL hare[NX][NY];
-  REAL puma[NX][NY];
-  int Map[NX][NY];
+    eqn_obj.time_interval= 0.4;
+    eqn_obj.prey_pop_inc_rate = 0.08;
+    eqn_obj.pred_rate_coeff = 0.04;
+    eqn_obj.rep_rate_pred = 0.02;
+    eqn_obj.pred_mort_rate = 0.06;
+    eqn_obj.diff_rate_hares = 0.2;
+    eqn_obj.diff_rate_pumas = 0.2;
 
+    //fscanf(file_Map, "%d %d", &nx, &ny);
+    nx=4;
+    ny=4;
 
-  /* Scan in all element of the array */
-  for (i = 0; i < NX; i++){
-    for (j = 0; j < NY; j++){
-      fscanf(file_Solution, "%d", &value_s);
-      fscanf(file_Hares, "%d", &value_h);
-      fscanf(file_Puma, "%d", &value_p);
-      fscanf(file_Map, "%d", &value_m);
-      solution[i][j] =2;// value_s;
-      hare[i][j] =2;// value_h;
-      puma[i][j] = 1;//value_p;
-      Map[i][j] = 1;//value_m;
-
-
+    /* Scan in all element of the array */
+    for (i = 0; i < NX; i++){
+        for (j = 0; j < NY; j++){
+            fscanf(file_solution, "%d", &value_s);
+            fscanf(file_hares, "%d", &value_h);
+            fscanf(file_puma, "%d", &value_p);
+            fscanf(file_map, "%d", &value_m);
+            solution[i][j] =2;// value_s;
+            hare[i][j] =2;// value_h;
+            puma[i][j] = 1;//value_p;
+            map[i][j] = 1;//value_m;
+        }
     }
-  }
 
+    //for (i = 0; i < max_iter; i++)
+    // compute(hare, puma, map, nx, ny, &eqn_obj);
 
-
-
-  //for (i = 0; i < max_iter; i++)
-  // compute(hare, puma, map, nx, ny, &eqn_obj);
-
-   for(i = 0; i < nx; i++){
-     for(j =0; j < ny; j++)
-       CU_ASSERT_EQUAL(solution[i][j], hare[i][j]);
-   }
+     for(i = 0; i < nx; i++){
+        for(j =0; j < ny; j++) {
+            CU_ASSERT_EQUAL(solution[i][j], hare[i][j]);
+        }
+    }
 
 }
-
-
-
-
 
 /* The main() function for setting up and running the tests.
  * Returns a CUE_SUCCESS on successful running, another
@@ -176,7 +179,7 @@ int main()
    /* add the tests to the suite */
    /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
    if (!CU_add_test(pSuite, "test of function readmap in I/O", test_readmap) ||
-       0)//!CU_add_test(pSuite, "test of kernel code", test_kernel))
+       !CU_add_test(pSuite, "test of kernel code", test_kernel))
    {
       CU_cleanup_registry();
       fputs(CU_get_error_msg(), stderr);
@@ -187,6 +190,9 @@ int main()
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
    CU_cleanup_registry();
+
+   if (CU_get_error())
+       fputs("Some tests FAILED", stderr);
    return CU_get_error();
 }
 
